944e2ee0b51f66c14f394c9dc35f9dd70afc4425
Deni Adhi <deni.adhi@y7mail.com>
2020-06-22 17:08:31 +0700

    Update Process.js

--- script/page-component/Process.js
+++ script/page-component/Process.js
@@ -7,10 +7,34 @@ class Process {
     ID = "process-content"
 
     async loadModel() {
-        const model = await tf.loadLayersModel('../../model/model.json');
+        //const model = await tf.loadLayersModel('../../model/model.json');
+    }
+	
+    tobase64Image() {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('#tes').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(this.image); // convert to base64 string
     }
 
 	async predict() {
+		const logits = tf.tidy(() => {
+			
+			// Pre-process the image
+			console.log( "Loading image..." );
+			const IMAGE_SIZE = 227;
+			const normalizationConstant = 1.0 / 255.0;
+			let img = tf.browser.fromPixels($("<img src=" + this.tobase64Image() + ">"), 1)
+				.resizeBilinear([IMAGE_SIZE, IMAGE_SIZE], false)
+				.expandDims(0)
+				.toFloat()
+				.mul(normalizationConstant);
+				
+			return model.predict(img);
+		});
+		
+		
 		this.loadModel().then(function() {
 		// disini nanti predictnya
 		});
